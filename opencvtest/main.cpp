698c4068a4db744bd19068894504fc3ba650d46d
odkey <amglowez@gmail.com>
2019-01-10 19:25:52 +0900

    Initial Commit

---
+++ opencvtest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opencvtest
+//
+//  Created by 尾髙 陽太 on 2019/01/10.
+//  Copyright © 2019 y-odaka. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
